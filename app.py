a6620bfe9b2453bf340719c7e61e77f49d7875ed
shahul <abc@xyz.com>
2025-05-28 09:34:34 +0530

    fourth versi`on

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-a = int(input('Enter a number'))
-if a % 2 == 0:
+a = int(input('Enter a number')) # taking input 
+if a % 2 == 0: # logic to check even or odd
   print('even')
 else:
   print('odd')
